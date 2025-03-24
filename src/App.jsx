041301b5bb629211ccbdd55ba1fa53a772278cde
Patrycja Gajewska <patrycjagajewska023@gmail.com>
2025-03-24 18:04:54 +0000

    jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 function App() {
+  const name = "Star Gazers"
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div>
+      <article>
+        <img src="/images/Star_Gazers.webp" width="700"></img>
+        <hgroup>
+          <h1>Meet the <i style={{color: "steelBlue"}}>{ name }</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button style={{backgroundColor: "steelblue", color: "white"}} onClick={() => alert("HI!")}>Click me</button>
+        </hgroup>
+      </article>
+    </div>
   )
 }
 export default App
